e9ebd1b239b4b2f77ac5905ddb52703d3ea622b0
seancote <sccote@gmail.com>
2019-06-06 18:36:13 -0400

    current version

---
+++ airQualiD/src/app/home/home.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  myChart: Chart;
+  @ViewChild('chartContainer') chartcontainer: ElementRef;
+  @ViewChild('chartcanvas') chartcanvas: ElementRef;
+
+ngAfterViewInit() {
+
+    this.createChart();
+    createChart(); {
+    this.myChart = new Chart(this.chartcanvas.nativeElement, {
+      type: 'horizontalBar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  }
+
+}
