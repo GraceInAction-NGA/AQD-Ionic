2a3618b69a863286dcbcbc1993130b9e3a7adc2e
Ar1elED <56279196+Ar1elED@users.noreply.github.com>
2021-07-15 17:18:47 -0400

    updated content on PM 2.5

--- src/app/home/components/popover/popover.component.ts
+++ src/app/home/components/popover/popover.component.ts
@@ -86,10 +86,16 @@ export class Popover {
   
   PM25Info: any = [{
     title:"What Is PM2.5?",
+    text:"PM2.5 is extremely small particles and droplets of pollution that because of their small size can penetrate deeply into lungs.",
+    deepDive:"Airborne particulate matter, also known as PM or particle pollution, is a mixture of extremely small particles and liquid droplets that can include acids, organic chemicals, metals, soil and dust particles, and biological matter such as fungal spores. Smaller particles pose a health concern because they can be inhaled into and accumulate in the lungs. Particles less than 2.5 micrometers in diameter, called PM2.5 or “fine” particulate matter, pose the greatest health risks. Because of their small size (about 1/30th the width of a human hair), these tiny particles penetrate deeply into the lungs."
   },{
     title:"Why It Matters",
+    text:"Exposure to PM, particularly PM2.5, can cause or worsen a number of diseases and can cause death.  PM2.5 can increase your risk of the following:",
+    list:["Lung irritation, coughing, and difficulty breathing","Asthma attacks and hospitalizations– especially children","Adverse birth outcomes, including premature births and low birth weight babies","Decreased lung function and impaired lung growth in children and teenagers","Increased blood pressure","Heart attacks and irregular heartbeat","Cancer","Death"]
   },{
     title:"Local Sources",
+    text:"",
+    list:[""]
   },{
     title:"Pollutant Index",
   },{
